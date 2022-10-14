176a83d400c424d1a3cb08c10c94789e74fe5233
mistiry <php.circuit.bot@gmail.com>
2022-10-14 17:11:41 -0500

    added doActionFromCommand module

---
+++ modules/doActionFromCommand/module.php
@@ -0,0 +1,22 @@
+<?php
+function doActionFromCommand($ircdata) {
+    //We need to pull in the socket object so we can directly send an ACTION
+    //with the proper control characters.
+    global $socket;
+    global $ircdata;
+
+    $options = parse_ini_file("./modules/doActionFromCommand/module.conf");
+
+    //Determine what to send
+    switch($options['action']) {
+        case "%COMMANDARGS%":
+            fputs($socket, "PRIVMSG ".$ircdata['location']." :\001ACTION ".$ircdata['commandargs']."\001\n");
+            break;
+        case "%USERNICKNAME%":
+            fputs($socket, "PRIVMSG ".$ircdata['location']." :\001ACTION ".$ircdata['usernickname']."\001\n");
+            break;
+        default:
+            fputs($socket, "PRIVMSG ".$ircdata['location']." :\001ACTION ".$options['action']."\001\n");
+    }
+    return true;
+}

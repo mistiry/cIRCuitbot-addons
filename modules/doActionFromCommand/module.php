784b381f54353d12e18c0224b43937711fb2739d
mistiry <php.circuit.bot@gmail.com>
2022-10-14 17:21:16 -0500

    removing call to global ircdata as it is passed into function by default

--- modules/doActionFromCommand/module.php
+++ modules/doActionFromCommand/module.php
@@ -3,7 +3,6 @@ function doActionFromCommand($ircdata) {
     //We need to pull in the socket object so we can directly send an ACTION
     //with the proper control characters.
     global $socket;
-    global $ircdata;
 
     $options = parse_ini_file("./modules/doActionFromCommand/module.conf");
 

a3905f2c94a0d7e9b53ed6180012c6be8f70b406
mistiry <php.circuit.bot@gmail.com>
2024-03-03 22:00:06 -0600

    trying more replacements to patch security hole with URL titles

--- triggers/parseURLfromMessage/trigger.php
+++ triggers/parseURLfromMessage/trigger.php
@@ -9,6 +9,7 @@ function parseURLfromMessage($ircdata) {
             $urltitle = urldecode($urltitle);
             $urltitle = html_entity_decode($urltitle);
             $urltitle = str_replace(array('\r','\r\n','\n','\n\r'), '', $urltitle);
+            $urltitle = preg_replace(array('\r','\r\n','\n','\n\r'),'', $urltitle);
             if(strlen($urltitle)>5 && strlen($urltitle)<450) {
                 $message = "".$ircdata['usernickname']." - URL Title - ".$urltitle."";
                 sendPRIVMSG($config['channel'], "".$message."");                        
@@ -22,6 +23,7 @@ function parseURLfromMessage($ircdata) {
             $urltitle = urldecode($urltitle);
             $urltitle = html_entity_decode($urltitle);
             $urltitle = str_replace(array('\r','\r\n','\n','\n\r'), '', $urltitle);
+            $urltitle = preg_replace(array('\r','\r\n','\n','\n\r'),'', $urltitle);
             if(strlen($urltitle)>5 && strlen($urltitle)<450) {
                 $message = "".$ircdata['usernickname']." - URL Title - ".$urltitle."";
                 sendPRIVMSG($config['channel'], "".$message."");                        

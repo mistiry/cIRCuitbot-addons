24accd91a7c77be7270f10f0b12477df6ca09738
mistiry <php.circuit.bot@gmail.com>
2024-03-06 15:06:52 -0600

    debugging triviaSystem

--- modules/triviaSystem/initialize_trivia_table.sql
+++ modules/triviaSystem/initialize_trivia_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `trivia` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userhostname` varchar(128) DEFAULT NULL,
+  `lastusednickname` varchar(64) DEFAULT NULL,
   `scores` varchar(16384) DEFAULT NULL,
   `lastwintime` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)

b9d288800c0f8a49024fd8a1c5d3b1b97d69bb70
mistiry <php.circuit.bot@gmail.com>
2024-07-12 23:53:34 -0400

    trying to strip the weird characters injected but the relay bot

--- modules/quoteSystem/module.php
+++ modules/quoteSystem/module.php
@@ -25,8 +25,8 @@ function getQuote($data) {
             $id = $row['id'];
             $submittedby = $row['submittedby'];
             $quote = $row['quote'];
-            $pattern = '/[^\P{C}\x20-\x7E\x{0080}-\x{FFFF}]/u';
-            $quote = preg_replace($pattern, '', $quote);
+            //$pattern = '/[^\P{C}\x20-\x7E\x{0080}-\x{FFFF}]/u';
+            //$quote = preg_replace($pattern, '', $quote);
             $timestamp = $row['timestamp'];
             $upvotes = $row['upvotes'];
             $downvotes = $row['downvotes'];

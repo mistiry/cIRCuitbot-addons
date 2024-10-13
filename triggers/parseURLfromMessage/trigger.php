b7da842e411c2294dedb1d30bd5c0d0e0fd72fdc
mistiry <php.circuit.bot@gmail.com>
2024-10-12 20:42:19 -0400

    adding YT video URL parsing

--- triggers/parseURLfromMessage/trigger.php
+++ triggers/parseURLfromMessage/trigger.php
@@ -79,9 +79,7 @@ function getYouTubeInfo($youtubeAPIKey, $url) {
 
     // Get the response from the API
     $response = file_get_contents($apiUrl);
-    $videoData = json_decode($response, true);
-    logEntry($response);
-    logEntry(print_r($videoData));
+    $videoData = json_decode($response);
 
     if (isset($videoData['items']) && count($videoData['items']) > 0) {
         $videoInfo = $videoData['items'][0];

2bafb9b99f9b72cbf326f9d0e46048072d960214
mistiry <mistiry@gmail.com>
2026-04-27 14:18:08 -0400

    seenInfo: require 2+ char search, fix display of escaped query var

--- modules/seenInfo/module.php
+++ modules/seenInfo/module.php
@@ -2,13 +2,19 @@
 function getSeenInfo($ircdata) {
     global $dbconnection;
 
-    $who = trim($ircdata['commandargs']);
-    $who = mysqli_real_escape_string($dbconnection, $who);
-    $who = str_replace(['%', '_'], ['\\%', '\\_'], $who);
-    $query = "SELECT hostname,last_datatype,last_message,last_location,timestamp FROM known_users WHERE nick_aliases LIKE '%$who%' ORDER BY timestamp DESC LIMIT 1";
+    $whoDisplay = trim($ircdata['commandargs']);
+
+    if (strlen($whoDisplay) < 2) {
+        sendPRIVMSG($ircdata['location'], "Please provide at least 2 characters to search.");
+        return true;
+    }
+
+    $whoQuery = mysqli_real_escape_string($dbconnection, $whoDisplay);
+    $whoQuery = str_replace(['%', '_'], ['\\%', '\\_'], $whoQuery);
+    $query = "SELECT hostname,last_datatype,last_message,last_location,timestamp FROM known_users WHERE nick_aliases LIKE '%$whoQuery%' ORDER BY timestamp DESC LIMIT 1";
 
     $result = mysqli_query($dbconnection,$query);
-                
+
     if(mysqli_num_rows($result) > 0) {
         if(mysqli_num_rows($result) > 1) {
             $message = "This is odd, it seems that your search returned multiple results. I can't show you seen data at this time - sorry!";
@@ -23,13 +29,13 @@ function getSeenInfo($ircdata) {
 
                 switch($lastdatatype) {
                     case "PRIVMSG":
-                        $message = "User '".$who."' was last seen on ".$timelastseen." using hostname '".$hostname."' saying \"".$lastmessage."\"";
+                        $message = "User '".$whoDisplay."' was last seen on ".$timelastseen." using hostname '".$hostname."' saying \"".$lastmessage."\"";
                         break;
                     case "QUIT":
-                        $message = "User '".$who."' was last seen on ".$timelastseen." using hostname '".$hostname."' quitting the channel.";
+                        $message = "User '".$whoDisplay."' was last seen on ".$timelastseen." using hostname '".$hostname."' quitting the channel.";
                         break;
                     case "JOIN":
-                        $message = "User '".$who."' was last seen on ".$timelastseen." using hostname '".$hostname."' joining the channel.";
+                        $message = "User '".$whoDisplay."' was last seen on ".$timelastseen." using hostname '".$hostname."' joining the channel.";
                         break;
                 }
             }
